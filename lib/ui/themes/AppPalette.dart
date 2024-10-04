118cc1eb6625b935c42dbebbc3584675e9029352
graccon <sujin.noh01@gmail.com>
2024-10-05 08:59:58 +0900

    feat: initialize Flutter app project

---
+++ lib/ui/themes/AppPalette.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppPalette {
+  // 주요 색상 (Primary, Secondary)
+  static const Color primaryColor = Color(0xFF0BAB7C); // 메인 색상
+  static const Color primaryColorHighlight = Color(0xFFE3F2EE); // 메인 하이라이트
+
+  static const Color secondaryColor = Color(0xFFF7825E); // 서브 메인 색상
+  static const Color secondaryColorHighlight = Color(0xFFFBE3CD); // 서브 하이라이트
+
+  // 경고 색상
+  static const Color warningColor = Color(0xFFE83E3E); // 경고 색상
+  static const Color warningColorHighlight = Color(0xFFF4E1E1); // 경고 하이라이트
+
+  // 배경 및 중립 색상
+  static const Color backgroundColor = Color(0xFFF5F5F5); // 배경 색상
+  static const Color whiteColor = Color(0xFFFEFEFE); // 흰색
+  static const Color blackColor = Color(0xFF161616); // 검은색
+
+  // 회색 계열
+  static const Color grayColor = Color(0xFF7C7C7C); // 회색
+  static const Color lightGrayColor = Color(0xFFD9D9D9); // 연한 회색
+  static const Color shapeBackgroundColor = Color(0xFFE9E9E9); // 도형 배경 회색
+}
