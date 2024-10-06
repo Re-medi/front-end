e8f60127feeec88d576da6219524a7fa6d16a325
graccon <sujin.noh01@gmail.com>
2024-10-06 13:48:15 +0900

    feat(DiseaseSelectionPage): 질병  아이콘 세트 추가
    - cardio, derm, endo, ent, gastro, gu, hema, infectious, msk, neuro, oncoimmune, respiratory 아이콘 추가

--- lib/ui/themes/IconAssets.dart
+++ lib/ui/themes/IconAssets.dart
@@ -16,4 +16,101 @@ class IconAssets {
       height: size,
     );
   }
+
+  // disease icons
+  static SvgPicture iconInfectious({double size = 60, Color? color}) {
+    return SvgPicture.asset(
+        'assets/icons/icon_infectious.svg',
+      height: size,
+      colorFilter: color != null ? ColorFilter.mode(color, BlendMode.srcIn) : null
+    );
+  }
+
+  static SvgPicture iconCardio({double size = 60, Color? color}) {
+    return SvgPicture.asset(
+        'assets/icons/icon_cardio.svg',
+        height: size,
+        colorFilter: color != null ? ColorFilter.mode(color, BlendMode.srcIn) : null
+    );
+  }
+
+  static SvgPicture iconDerm({double size = 60, Color? color}) {
+    return SvgPicture.asset(
+        'assets/icons/icon_derm.svg',
+        height: size,
+        colorFilter: color != null ? ColorFilter.mode(color, BlendMode.srcIn) : null
+    );
+  }
+
+  static SvgPicture iconEndo({double size = 60, Color? color}) {
+    return SvgPicture.asset(
+        'assets/icons/icon_endo.svg',
+        height: size,
+        colorFilter: color != null ? ColorFilter.mode(color, BlendMode.srcIn) : null
+    );
+  }
+
+  static SvgPicture iconEnt({double size = 60, Color? color}) {
+    return SvgPicture.asset(
+        'assets/icons/icon_ent.svg',
+        height: size,
+        colorFilter: color != null ? ColorFilter.mode(color, BlendMode.srcIn) : null
+    );
+  }
+
+  static SvgPicture iconGastrp({double size = 60, Color? color}) {
+    return SvgPicture.asset(
+        'assets/icons/icon_gastro.svg',
+        height: size,
+        colorFilter: color != null ? ColorFilter.mode(color, BlendMode.srcIn) : null
+    );
+  }
+
+  static SvgPicture iconGu({double size = 60, Color? color}) {
+    return SvgPicture.asset(
+        'assets/icons/icon_gu.svg',
+        height: size,
+        colorFilter: color != null ? ColorFilter.mode(color, BlendMode.srcIn) : null
+    );
+  }
+
+  static SvgPicture iconHema({double size = 60, Color? color}) {
+    return SvgPicture.asset(
+        'assets/icons/icon_hema.svg',
+        height: size,
+        colorFilter: color != null ? ColorFilter.mode(color, BlendMode.srcIn) : null
+    );
+  }
+
+  static SvgPicture iconMsk({double size = 60, Color? color}) {
+    return SvgPicture.asset(
+        'assets/icons/icon_msk.svg',
+        height: size,
+        colorFilter: color != null ? ColorFilter.mode(color, BlendMode.srcIn) : null
+    );
+  }
+
+  static SvgPicture iconNeuro({double size = 60, Color? color}) {
+    return SvgPicture.asset(
+        'assets/icons/icon_neuro.svg',
+        height: size,
+        colorFilter: color != null ? ColorFilter.mode(color, BlendMode.srcIn) : null
+    );
+  }
+
+  static SvgPicture iconOncoimmune({double size = 60, Color? color}) {
+    return SvgPicture.asset(
+        'assets/icons/icon_oncoimmune.svg',
+        height: size,
+        colorFilter: color != null ? ColorFilter.mode(color, BlendMode.srcIn) : null
+    );
+  }
+
+  static SvgPicture iconRespiratory({double size = 60, Color? color}) {
+    return SvgPicture.asset(
+        'assets/icons/icon_respiratory.svg',
+        height: size,
+        colorFilter: color != null ? ColorFilter.mode(color, BlendMode.srcIn) : null
+    );
+  }
 }
