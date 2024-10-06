03f8fa94613330dc59d70eed357432c36ecf55c9
graccon <sujin.noh01@gmail.com>
2024-10-06 12:52:12 +0900

    Update main.dart and Podfile for iOS and macOS setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart'; // 여기 추가
+import 'package:remedi/ui/screens/DiseaseSelectionPage.dart';
 import 'package:remedi/ui/themes/AppPalette.dart';
+import 'package:remedi/ui/screens/UserDashboardPage.dart';
+import 'package:remedi/data/models/PracticeState.dart'; // PracticeState 추가
+
 void main() {
   runApp(const MyApp());
 }
@@ -7,69 +12,23 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Re:medi',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: AppPalette.primaryColor),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Scaffold(
-      appBar: AppBar(
-
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-
-        title: Text(widget.title),
-      ),
-      body: Center(
-
-        child: Column(
-
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return ChangeNotifierProvider(
+      create: (context) => PracticeState(),
+      child: MaterialApp(
+        title: 'Re:medi',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: AppPalette.primary),
+          scaffoldBackgroundColor: AppPalette.background,
+          useMaterial3: true,
         ),
+        initialRoute: '/',
+        routes: {
+          '/': (context) => const UserDashboardPage(),
+          '/disease_selection': (context) => DiseaseSelectionPage(),
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
